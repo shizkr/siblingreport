a35d8b5e47a4fb2898f23141a471b2de0e8b91e5
Denny Yang <denny.ds.yang@gmail.com>
2025-07-06 22:18:29 -0700

    add daily word

---
+++ daily_support_me.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+import telegram
+
+if os.getenv("GITHUB_ACTIONS") != "true":
+    load_dotenv()  # 로컬 환경일 경우에만 .env 파일을 불러옵니다
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+# 환경 변수 로드 (GitHub Actions에서는 Secrets로 자동 주입됨)
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+# OpenAI 클라이언트 생성
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def tg_msg(msg):
+    telegram.send_message_to_telegram(BOT_TOKEN, CHAT_ID, msg)
+
+## Question to ChatGPT
+system_question = "AI  내 개인 비서처럼 작성해줘."
+user_question = '''인생에 힘이 되는 따뜻하고 철학적이며 투자에 도움이 되는 명언 한마디 알려줘!'''
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": system_question},
+        {"role": "user", "content": user_question}
+    ],
+    temperature=0.8
+)
+answer = response.choices[0].message.content
+tg_msg(answer)
